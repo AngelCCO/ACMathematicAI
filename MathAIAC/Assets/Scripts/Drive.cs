5453f00f6fe45c67bdb884cf6ba9b5506dfa4526
AngelCCO <116102695+AngelCCO@users.noreply.github.com>
2023-11-29 12:19:20 -0800

    Al,ost
    e

--- MathAIAC/Assets/Scripts/Drive.cs
+++ MathAIAC/Assets/Scripts/Drive.cs
@@ -7,6 +7,7 @@ public class Drive : MonoBehaviour
     public float speed = 10.0f;
     public float rotationSpeed = 100.0f;
     public GameObject fuel;
+    bool autoPilot = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,24 +23,42 @@ public class Drive : MonoBehaviour
         float dot = tF.x * fD.x + tF.y * fD.y;
         float angle = Mathf.Acos(dot / (tF.magnitude * fD.magnitude));
 
-        Debug.Log("Angle:" + angle * Mathf.Rad2Deg);
-        Debug.Log("unity Angle:" + Vector3.Angle(tF, fD));
-
-        Debug.DrawRay(this.transform.position, tF, Color.green, 2);
+        Debug.DrawRay(this.transform.position, tF * 10, Color.green, 2);
         Debug.DrawRay(this.transform.position, fD, Color.red, 2);
 
-        this.transform.Rotate(0, 0, angle * Mathf.Rad2Deg);
+        int clockwise = 1;
+        if (Cross(tF, fD).z < 0)
+            clockwise = -1;
+
+        this.transform.Rotate(0, 0, (angle * clockwise * Mathf.Deg2Rad) * 0.2f);
+    }
+
+    Vector3 Cross(Vector3 v, Vector3 w)
+    {
+        float xMult = v.y * w.z - v.z * w.y;
+        float yMult = v.z * w.x - v.x * w.z;
+        float zMult = v.x * w.y - v.y * w.x;
+
+        Vector3 crossPod = new Vector3(xMult, yMult, zMult);
+        return crossPod;
     }
     void CalculateDistance()
     {
         Vector3 tP = this.transform.position;
         Vector3 fP = fuel.transform.position;
 
-        float distance = Mathf.Sqrt(Mathf.Pow(tP.x - tP.x, 2) + Mathf.Pow(tP.y - fP.y, 2));
+        float distance = Mathf.Sqrt(Mathf.Pow(tP.x - tP.x, 2) + Mathf.Pow(tP.y - fP.y, 2) + Mathf.Pow(tP.z - fP.z, 2));
 
         float unityDistance = Vector3.Distance(tP, fP);
             Debug.Log("Distance:" + distance);
         Debug.Log("Unity Distance" + unityDistance);
+    }
+
+    float autoSpeed = 0.1f;
+    void AutoPilot()
+    {
+        CalculateAngle();
+        this.transform.Translate(this.transform.up * autoSpeed, Space.World);
     }
 
     // Update is called once per frame
@@ -57,5 +76,12 @@ public class Drive : MonoBehaviour
             CalculateDistance();
             CalculateAngle();
         }
+
+        if (Input .GetKeyUp(KeyCode.T))
+        {
+            autoPilot = !autoPilot;
+        }
+        if(autoPilot)
+            AutoPilot();
     }
 }
